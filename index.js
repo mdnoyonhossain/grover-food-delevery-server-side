1b008fd6ec3de7d715f143e861bca815367ac429
MD Shahinur Rahman Nayon <mdshahinurrahmannayon@gmail.com>
2022-11-08 15:02:19 +0600

    create operation and read operation done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,10 +11,39 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n3a0m.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    const servicesCollection = client.db('Grover').collection('services');
+
+    app.get('/services', async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.post('/services', async (req, res) => {
+      const services = req.body;
+      const result = await servicesCollection.insertOne(services);
+      res.send(result)
+    })
+  }
+  finally {
+
+  }
+}
+
+run().catch(error => console.log(error))
+
 app.get('/', (req, res) => {
-    res.send('Quick Eat Server is Running');
+  res.send('Quick Eat Server is Running');
 });
 
 app.listen(port, () => {
-    console.log(`Quick Eat Server on port ${port}`);
+  console.log(`Quick Eat Server on port ${port}`);
 });
